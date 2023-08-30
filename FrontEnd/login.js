38916eb9d87e7e2f76a7053e0222304f1a98f5a0
Lucas Leib <lucas.leib@gmail.com>
2023-08-30 15:13:56 +0200

    Sophie Bluel v0.1.1 (essai arranger poblème login)

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -11,16 +11,53 @@
 
 // verifierString()
 
+const formulaire = document.getElementById('login')
+
+formulaire.addEventListener('submit', async function(event) {
+    event.preventDefault()
+
+    const email = document.getElementById('email').value
+    const motdepasse = document.getElementById('password').value
+
+    console.log('Email : ', email)
+    console.log('Mot de passe : ', motdepasse)
+
+    const donnees = {
+        "email": "user@example.com",
+        "password": "motdepasse"
+    }
+    try {
+        const response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            body: JSON.stringify(donnees),
+            headers: {"Content-Type": "application/json"}
+            })
+
+        if (!response.ok) {
+            throw new Error('La requête a échoué')
+        }
+
+        const data = await response.json()
+        console.log('Réponse du serveur : ', data)
+
+    } catch (error) {
+        console.log('Erreur : ', error)
+    }
+    
+
+    // .then(response => response.json())
+    // .then(data => {
+    //     // Traitez la réponse du serveur ici
+    //     console.log('Réponse du serveur : ', data);
+    // })
+    // .catch(error => {
+    //     // Gérez les erreurs ici
+    //     console.error('Erreur : ', error);
+    // });
+
+})
 
 
 
-const verif = {
-    "email": "string",
-    "password": "string"
-}
 
-    fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        body: JSON.stringify(verif),
-        headers: {"Content-Type": "application/json"}
-    })
+
