5d7029c489b4ea8e042dcff088b3cb64d743b5bd
Lucas Leib <lucas.leib@gmail.com>
2023-09-04 22:21:56 +0200

    Sophie Bluel v0.6 (ajout de l'icone poubelle)

--- FrontEnd/modules-edit/galerie-edit.js
+++ FrontEnd/modules-edit/galerie-edit.js
@@ -1,19 +1,29 @@
 export function genererGalerieSansTitre(data){
+    const sectionGallery = document.querySelector(".gallery-edit")
     for (let i = 0; i < data.length; i++) {
         const article = data[i]
-
-        const sectionGallery = document.querySelector(".gallery-edit")
+        
         const pieceElement = document.createElement("figure")
+
+        const imageDiv = document.createElement("div")
+        imageDiv.classList.add("image-div")
 
         const imageElement = document.createElement("img")
         imageElement.src = article.imageUrl
 
+        const poubelleElement = document.createElement('i')
+        poubelleElement.classList.add("fas", "fa-trash-can")
+
         const editElement = document.createElement("p")
         editElement.innerText = "éditer"
 
-        sectionGallery.appendChild(pieceElement)
-        pieceElement.appendChild(imageElement)
+        imageDiv.appendChild(imageElement)
+        imageDiv.appendChild(poubelleElement)
+
+        pieceElement.appendChild(imageDiv)
         pieceElement.appendChild(editElement)
+
+        sectionGallery.appendChild(pieceElement)
 
         }
     }
